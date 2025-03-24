afeefa3b29987a566bace5cfb48885389b3ef87e
jiin <jyl08840@naver.com>
2025-03-24 17:53:20 +0900

    JAVASCRIPT프로젝트2-5

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 //DOM 요소 연결을 시켜줘야한다.
-const screen = document.querySelector("#screen");
+const screen = document.getElementById("screen");
+
 const buttons = document.querySelectorAll("button");
 //document.querySelector();=>태그한개만 가지고 와서 변수에 담아준다.
 //document.querySelectorAll();=>태그 여러개를 리스트형태로 담아서 변수에 담아준다.
 
-let currentInput = "";
-//아무것도 없는 빈캆 설정
 
 
-const reOperator = /^(\d+|\*\*|[+\-*/])$/; // 사칙연산자(+,-,*,/)를 구별하게 해주는 정규표현식
-const reNumber = /[0-9]/g; // 숫자를 구별해주는 정규표현식
+
+const operatorRegex = /^[+\-*/]$/; // 연산자 구별 정규식
+const numberRegex = /^[0-9]$/; // 숫자 구별 정규식
+
+
 
 //input태그 화면에 숫자 또는 연산자를 추가하는 함수
 function appendToScreen(value){
@@ -21,29 +23,89 @@ function appendToScreen(value){
 //화면 초기화 함수
 function clearScreen(){
   screen.value = "";
-  //빈 인풋값
+
 }
 
-//연산 수행 함수
-function calcuate(operator, numbers){
-  const[num1, num2] = numbers.map(Number);
-  //number에다가 배열로 된 데이터들을 넣을거임(숫자와 연산자)
-  //map->numbers라는 기능을 필터링해서 numbers를 다시 정리해줌
-  //numbers.map(Number); =>numbers안에 있는 배열데이터들을 전부 숫자화 시켜달라는 뜻
-  //지금은 문자로 되어있음
+// 연산 수행 함수
+function calculate(operator, numbers) {
+  const [num1, num2] = numbers.map(Number);
+  //numbers에다가 배열로 된 데이터들을 넣는 것(숫자와 문자)
+  //numbers.map(Number);=>numbers안에 있는 배열 데이터들을 숫자로 바꿔준다.
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num2 !== 0 ? num1 / num2 : "Error"; 
+      //삼항조건식
+      //num2가 0이 아닌게 true이면 num1/num2
+      //num2가 0이 아닌게 false이면 "Error"가 실헹이 된다.
+    default:
+      return ""; 
+  }
 }
-    switch(operator){
-      case "+":
-        return num1 + num2;
-      case "-":
-        return num1 - num2;
-      case "*":
-        return num1 * num2;
-      case "/":
-        return num2 !== 0 ? num1 / num2 : "Error"; 
-       //삼항조건식
-       //만약 num2를 0이 아닌게 true이면면 num1/num2를 해주고
-       // num2가 0이 아닌게 false면 Error를 출력해준다.
-      default:
-        return "";
-    }
+
+  //버튼 클릭 시 동작을 처리하는 함수
+  function handleButtonClick(event) {
+    console.log(event);
+    event.preventDefault();
+    //새로고침되는게 방지가 된다.
+    const buttonText = event.target.innerText;
+    //event.target.innerText => 버튼의 텍스트를 가져온다.
+
+     if(numberRegex.test(buttonText)==true){
+       //숫자를 눌렀을때
+       appendToScreen(buttonText);
+     }else if(operatorRegex.test(buttonText)==true){
+       //연산자를 눌렀을때
+       appendToScreen(buttonText);
+  }
+  }
+  
+//버튼 클릭 이벤트 리스너 등록 함수
+function initializeButtonListeners(){
+  buttons.forEach((button)=>{
+   button.addEventListener("click", handleButtonClick);
+   
+  })
+}
+
+//"=" 버튼 클릭 시 계산 결과를 화면에 표시
+function handleResultClick(){
+  const screenValue = screen.value;
+  //화면에 있는 값을 가져온다.
+
+
+  if(screenValue.includes("+")){
+    const [num1,num2] = screenValue.split("+");
+    //split("+");=>+를 기준으로 나눠준다.
+    screen.value = calculate("+",[num1,num2]);
+} else if (screenValue.split("-")) {
+  const [num1,num2] = screenValue.split("-");
+  screen.value = calculate("-",[num1,num2]);
+
+} else if (screenValue.split("*")) {
+  const [num1,num2] = screenValue.split("*");
+  screen.value = calculate("*",[num1,num2]);
+
+} else if (screenValue.split("/")) {
+  const [num1,num2] = screenValue.split("/");
+  screen.value = calculate("/",[num1,num2]);
+}
+
+
+}
+
+// 초기화 버튼 클릭 시 화면을 초기화
+document.getElementById("resetButton").addEventListener("click", function () {
+  clearScreen();
+});
+
+//"="버튼 클릭 시 계산실행
+document.getElementById("result").addEventListener("click",handleResultClick);
+
+//계산기 기능 실행
+initializeButtonListeners();
